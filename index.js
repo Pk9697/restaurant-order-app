c46c0fadaeeab94f9213371fe1e32fa582b5a156
Prashant Kumar <prashant.aman9@gmail.com>
2022-11-07 21:23:34 +0530

    complete modal logic and wrap up

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
 import { menuArray } from "./data.js";
 
 let ordersArray = [];
+const cardForm=document.getElementById('card-details-form')
+const orderSuccess=document.getElementById('order-success');
 document.addEventListener("click", function (e) {
+    e.preventDefault()
   // console.log(e.target.dataset)
-
   if (e.target.dataset.addorder) {
-    // console.log(e.target.dataset.addorder)
     addItemToOrders(e.target.dataset.addorder);
+    orderSuccess.style.display='none'
   } else if (e.target.dataset.removeorder) {
     removeItemFromOrders(e.target.dataset.removeorder);
+  } else if(e.target.id==='complete-order-btn'){
+    document.getElementById('modal').style.display="flex"
+  } else if(e.target.id==='pay-btn'){
+    const formData=new FormData(cardForm)
+    document.getElementById('modal').style.display="none"
+    ordersArray=[]
+    renderOrders()
+    orderSuccess.style.display='flex'
+    document.getElementById('order-success').innerHTML=`
+        <h2>Thanks, ${formData.get('name')}! Your order is on its way!</h2>
+    `
   }
 });
+
+
 
 function removeItemFromOrders(orderId) {
   ordersArray = ordersArray.filter((order) => order.id != orderId);
