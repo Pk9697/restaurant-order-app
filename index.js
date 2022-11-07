a1f9540045953de843f6a2dd4e483a9a3efd95e9
Prashant Kumar <prashant.aman9@gmail.com>
2022-11-07 18:20:05 +0530

    complete required logic for the project

--- index.js
+++ index.js
@@ -1,6 +1,58 @@
 import { menuArray } from "./data.js";
 
-console.log(menuArray);
+let ordersArray = [];
+document.addEventListener("click", function (e) {
+  // console.log(e.target.dataset)
+
+  if (e.target.dataset.addorder) {
+    // console.log(e.target.dataset.addorder)
+    addItemToOrders(e.target.dataset.addorder);
+  } else if (e.target.dataset.removeorder) {
+    removeItemFromOrders(e.target.dataset.removeorder);
+  }
+});
+
+function removeItemFromOrders(orderId) {
+  ordersArray = ordersArray.filter((order) => order.id != orderId);
+  renderOrders();
+  
+}
+
+function addItemToOrders(orderId) {
+  orderId = parseInt(orderId);
+  const getItemOrderObj = menuArray.filter((menu) => menu.id === orderId)[0];
+  ordersArray.unshift(getItemOrderObj);
+  renderOrders();
+
+}
+
+function renderOrders() {
+  let ordersHtml = "";
+  let totalPrice = 0;
+  
+  ordersArray.forEach((order) => {
+    ordersHtml += `
+            <div class="each-order">
+                <div class="each-order-name">
+                    <h2>${order.name}</h2>
+                    <button data-removeorder=${order.id}>remove</button>
+                </div>
+                <h2>$${order.price}</h2>
+            </div>
+        `;
+    totalPrice += order.price;
+  });
+
+  document.getElementById("total-orders").innerHTML = ordersHtml;
+  document.getElementById("total-price").textContent = `$${totalPrice}`;
+  
+  if(totalPrice>0){
+    document.getElementById('orders-container').style.display="block"
+  }else{
+    document.getElementById('orders-container').style.display="none"
+
+  }
+}
 
 function getMenuHtml() {
   let menuHtml = "";
@@ -12,11 +64,11 @@ function getMenuHtml() {
                 <div class="menu-details">
                     <h2>${menu.name}</h2>
                     <p>${menu.ingredients}</p>
-                    <h2>${menu.price}</h2>
+                    <h2>$${menu.price}</h2>
                 </div>
             </div>
-            <div class="menu-order-btn">
-                <i class="fa-thin fa-plus"></i>
+            <div class="menu-order-btn" data-addorder=${menu.id}>
+                <i class="fa-thin fa-plus" data-addorder=${menu.id}></i>
             </div>
         </div>
         `;
@@ -25,8 +77,8 @@ function getMenuHtml() {
 }
 
 function render() {
-//   console.log(getMenuHtml());
-  document.getElementById('main').innerHTML=getMenuHtml()
+  //   console.log(getMenuHtml());
+  document.getElementById("main").innerHTML = getMenuHtml();
 }
 
 render();
